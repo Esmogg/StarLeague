5fafc9a19ddb57f8ea73907bea05ec0f37356f6d
EsmoGG <tamuquilol@gmail.com>
2026-07-10 00:46:57 -0300

    v5.3 Reorganizacion de archivos
    Se movieron los archivos a carpetas que correspondan a sus extensiones.

---
+++ js/script.js
@@ -0,0 +1,66 @@
+/* Menu Lateral */
+const menuBtn = document.getElementById("menuBtn");
+const aside = document.querySelector("aside");
+const menuOverlay = document.getElementById("menuOverlay");
+
+function abrirCerrarMenu() {
+  aside.classList.toggle("abierto");
+  menuOverlay.classList.toggle("activo");
+  document.body.classList.toggle("menu-abierto");
+  document.documentElement.classList.toggle("menu-abierto");
+}
+
+menuBtn.addEventListener("click", abrirCerrarMenu);
+
+menuOverlay.addEventListener("click", () => {
+  aside.classList.remove("abierto");
+  menuOverlay.classList.remove("activo");
+  document.body.classList.remove("menu-abierto");
+  document.documentElement.classList.remove("menu-abierto");
+});
+
+/* Modo oscuro */
+const darkModeToggle = document.getElementById("darkModeToggle");
+
+/* Cargar preferencia guardada */
+if (localStorage.getItem("darkMode") === "true") {
+  document.body.classList.add("dark-mode");
+  document.documentElement.classList.add("dark-mode");
+
+  if (darkModeToggle) {
+    darkModeToggle.checked = true;
+  }
+}
+
+/* Cambiar tema */
+if (darkModeToggle) {
+  darkModeToggle.addEventListener("change", () => {
+    if (darkModeToggle.checked) {
+      document.body.classList.add("dark-mode");
+      document.documentElement.classList.add("dark-mode");
+
+      localStorage.setItem("darkMode", "true");
+    } else {
+      document.body.classList.remove("dark-mode");
+      document.documentElement.classList.remove("dark-mode");
+
+      localStorage.setItem("darkMode", "false");
+    }
+  });
+}
+
+/* Boton de subida de la pagina */
+const btnSubir = document.getElementById("btnSubir");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 65) {
+    console.log("Mostrar");
+    btnSubir.classList.add("visible");
+  } else {
+    btnSubir.classList.remove("visible");
+  }
+});
+
+btnSubir.addEventListener("click", () => {
+  window.scrollTo({ top: 0 });
+});
