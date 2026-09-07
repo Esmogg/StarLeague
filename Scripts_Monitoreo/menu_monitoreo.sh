edc0dacf2975b400906cc2cbeccced1766c0582e
ShadowNora <renzimar08@gmail.com>
2026-09-06 21:46:44 -0300

    Arreglo al menu

--- Scripts_Monitoreo/menu_monitoreo.sh
+++ Scripts_Monitoreo/menu_monitoreo.sh
@@ -4,15 +4,18 @@ clear
 echo "=== Ingresando al menu de monitoreo ==="
 echo "Desea: "
 echo "1- Administrar procesos"
-echo "2- Gestionar servicios"
-echo "3- Gestionar el firewall"
-echo "4- Salir"
+echo "2- Gestionar el firewall"
+echo "3- Salir"
 read -p "Ingrese una opcion" opcion
 
 case $opcion in
 1) ./procesos.sh ;;
-2) ./servicios.sh ;;
-3) ./firewall.sh ;;
-4) echo "saliendo" ;;
-*) echo "opcion invalida" sleep 3 ./menu_monitoreo.sh ;;
+2) ./firewall.sh ;;
+3) echo "saliendo"
+exit 0
+;;
+*) echo "opcion invalida" 
+sleep 3 
+./menu_monitoreo.sh
+;;
 esac
