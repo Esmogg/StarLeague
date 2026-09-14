e4181f3e716d49c8a83826b4acb890ac200d42cd
EsmoGG! <110956035+Esmogg@users.noreply.github.com>
2026-09-14 14:19:17 -0300

    Update respaldo_completo.sh

--- scripts/respaldos/respaldo_completo.sh
+++ scripts/respaldos/respaldo_completo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 FECHA=$(date +%Y%m%d)
 DESTINO="/home/respaldos"
-ORIGEN="/home/renzi"
+ORIGEN="/"
 
 tar -cvzf "$DESTINO/full_$FECHA.tar.gz" \
     --listed-incremental="$DESTINO/full.snar" \
